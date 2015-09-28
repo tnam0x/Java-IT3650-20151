9bc8584be8d448f19dfc9b7ee1a5166dd5a21f8c
Trần Xuân Nam <nam3vj@gmail.com>
2015-09-28 16:41:37 +0700

    update change

--- Demo Java/src/demo/Test.java
+++ Demo Java/src/demo/Test.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 class Test {
 	private static boolean flag;
+
 	public static void main(String[] args) {
 		StringBuilder strb = new StringBuilder();
 		System.out.println("Capacity: " + strb.capacity());
@@ -22,23 +23,24 @@ class Test {
 		String[] str = { "", "", "" };
 		str[0] += "*";
 		System.out.println(str[0]);
-		
+
 		//
-		ArrayList<Object> list = new  ArrayList<>();
-		if(list.isEmpty())
+		ArrayList<Object> list = new ArrayList<>();
+		if (list.isEmpty())
 			System.out.println("NULL");
 		else
 			System.out.println("Not NULL");
-		
+
 		//
 		boolean flags = false;
-		if(!flags)
+		if (!flags)
 			flags = true;
-		if(flags) {
+		if (flags) {
 			System.out.println("True");
 			flags = true;
 		}
-		if(flags)
+		if (flags)
 			System.out.println("Done!");
+		
 	}
 }
