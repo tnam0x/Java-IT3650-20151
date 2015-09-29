d66b6b874a28ea3c9905f83402e07fd3de31af9c
Trần Xuân Nam <nam3vj@gmail.com>
2015-09-29 17:05:11 +0700

    update change

--- Garden Centre/src/namtran/hust/interfaces/MainScreen.java
+++ Garden Centre/src/namtran/hust/interfaces/MainScreen.java
@@ -4,42 +4,44 @@ import java.text.DecimalFormat;
 
 public class MainScreen {
 	final static int MAX_SELLABLE = 100;
-	private static Sellable[] sellAble;
+	private static Sellable[] sellable;
 	private static double cost;
 	private static double totalVolume;
 
 	public static void main(String[] args) {
-		sellAble = new Sellable[MAX_SELLABLE];
+		sellable = new Sellable[MAX_SELLABLE];
 		int i = 0;
 		// add sellable
 		Item item = new Item("1234567890123", "Large Orange Screwdriver", 55.55);
-		sellAble[i++] = item;
+		sellable[i++] = item;
 		Timber timber = new Timber("Treated Pine", 18, 4, 3.25, 11);
-		sellAble[i++] = timber;
+		sellable[i++] = timber;
 		item = new Item("12312312", "Self Sealing Stem Bolts", 0.99);
-		sellAble[i++] = item;
+		sellable[i++] = item;
 		item = new Item("P100000000000", "White & Flattener Corded D", 123);
-		sellAble[i++] = item;
+		sellable[i++] = item;
 		timber = new Timber("Shellacked Wind", 2.8, 2.8, 12, 4);
-		sellAble[i++] = timber;
+		sellable[i++] = timber;
 		Rock rock = new Rock("Limestone", 212.5, 2);
-		sellAble[i++] = rock;
+		sellable[i++] = rock;
 		rock = new Rock("Medium Volcanic Rocks", 8.5, 6.6);
-		sellAble[i++] = rock;
+		sellable[i++] = rock;
 		Soil soil = new Soil("Loam", 30, 12.25, 10);
-		sellAble[i++] = soil;
+		sellable[i++] = soil;
 		soil = new Soil("River Sand", 40, 2.2, 2);
-		sellAble[i++] = soil;
+		sellable[i++] = soil;
 		// display
 		for (int j = 0; j < i; j++) {
-			System.out.println(sellAble[j].getReceiptLine());
-			cost += sellAble[j].getPrice();
+			System.out.println(sellable[j].getReceiptLine());
+			cost += sellable[j].getPrice();
 		}
 		// show cost
 		DecimalFormat df = new DecimalFormat(".##");
 		String str = "Total $" + df.format(cost);
 		System.out.println("---------------------------------------------------");
 		System.out.format("%51s", str);
+		System.out.println("\nVolume in delivery: " + totalVolume + " cubic metres");
+
 	}
 
 }
